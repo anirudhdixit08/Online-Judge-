be6410914bec2032de244e5584f23ac6801fab60
Anirudh Dixit <anirudhdixit.sjknp@gmail.com>
2025-11-02 05:32:05 +0530

    Added admin dashboard

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux'; 
 import Dashboard from '../components/dashboard/Dashboard';
+import AdminDashboard from '../components/dashboard/AdminDashboard';
 
 const Homepage = () => {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <div className="p-4 md:p-8 bg-base-200 min-h-screen">
-      <Dashboard />
+      {user?.role === 'admin' ? <AdminDashboard /> : <Dashboard />}
+
     </div>
   );
 };
