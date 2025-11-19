1b5ad13d46da22c9ddd2ccea7c0f9148a93c2f44
Anirudh Dixit <anirudhdixit.sjknp@gmail.com>
2025-11-19 16:28:06 +0530

    Added api option

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -109,7 +109,7 @@ export default function UpdateProfilePage() {
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  className="stroke-current shrink-0 h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                 >
@@ -125,7 +125,7 @@ export default function UpdateProfilePage() {
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  className="stroke-current shrink-0 h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                 >
